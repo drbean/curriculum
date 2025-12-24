b5925e9936ead1c476da3d82965ee2d9dd15744f
Dr Bean <drbean@freeshell.org>
2025-12-24 10:07:28 +0900

    ADSL Wi-Fiルーターの欲 しい 人！に上げます。詳細は http://drbean.sdf.org/WebCaster3100SV.html

---
+++ pages/mastodon/webcaster.sh
@@ -0,0 +1,8 @@
+#function web_caster () {
+#    # toot auth | sed -nE '/ACTIVE/s/^.+(@[^[:space:]]+)[[:space:]].+$/drbean\1/p' > ur_auth && toot activate drbean@mstdn.jp && echo 'ADSL Wi-Fiルーターの欲 しい 人！に上げます。詳細は http://drbean.sdf.org/WebCaster3100SV.html' | toot post && toot activate drbean@mastodon-japan.net && echo 'ADSL Wi-Fiルーターの欲しい人 ！に上げます。詳細は http://drbean.sdf.org/WebCaster3100SV.html' | toot post && toot activate $(< ur_auth))'
+#})'}
+
+offer="ADSL Wi-Fiルーターの欲 しい 人！に上げます。詳細は http://drbean.sdf.org/WebCaster3100SV.html"
+
+<<< $offer masto_master_messter mastodon-japan.net mstdn.jp
+
